1d8df29f4dced38360058a645710bab422b09252
RenRusal <mostapha.bazzaoui@gmail.com>
2018-12-23 14:58:06 +0000

    Create ints_and_floats.py

---
+++ ints_and_floats.py
@@ -0,0 +1,40 @@
+'''This time it's going to be all about integers and floats'''
+
+# what are ints and floats
+
+print(type(12))        # the output would be 'class <int>' int stand for integer
+print(type(12.1))      # this would give us 'class <float>' 
+print(type(12.0))      # no!! this isn't an integer it's a float
+print(type('13'))      #and this is also not an int it's string type 
+#we can change the type of an expression with some functions 
+var1 = 12.1
+var2 = '14.5'
+
+print(int(var1))      # the int function will change the type of the variable to an integer so the resulting output would be 12
+print(int(var2))      # the same case here, the output is : 14 not 14.5 
+print(float(13))      # the float function will change the type of the variable to a float so the resulting output would be 13.0
+print(str(15.4))      # the str function will change the type of the variable to a string so the resulting output would be '15.4'
+
+# Arithmetic Operators: 
+
+# Addition: 3 + 2 
+# Subtraction: 3 - 2 
+# Multiplication: 3 * 2 
+# Division: 3 / 2 
+# Floor Division: 3 // 2 
+# Exponent: 3 ** 2 
+# Modulus: 3 % 2
+
+# Comparisons: 
+
+# Equal: 3 == 2 
+# Not Equal: 3 != 2 
+# Greater Than: 3 > 2 
+# Less Than: 3 < 2 
+# Greater or Equal: 3 >= 2 
+# Less or Equal: 3 <= 2
+
+''' just try to print those examples stated beside the notations
+I just felt lazy. '''
+                                                                                                               #end.
+                                                                                  
